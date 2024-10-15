912c7f8b635e607aa16ee8f500c57f1c48dcfaa9
Mohau Mushi <david1mushi1@gmail.com>
2024-10-15 09:31:27 +0200

    Updated globals.css and add ProductGrid component to Home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,11 @@
+import ProductGrid from "@/components/ProductGrid";
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <div className="min-h-screen px-4 mt-5 text-gray-900 transition-colors duration-300">
+        <ProductGrid />
+      </div>
+    </>
   );
 }
