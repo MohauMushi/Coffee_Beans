5c902d2d2b215d91228728ded9e6d3f052be64b3
Mohau Mushi <david1mushi1@gmail.com>
2024-10-17 11:20:48 +0200

    Added 'Account' link to Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -117,6 +117,12 @@ const Navbar = () => {
             ) : (
               <>
                 <Link
+                  href="/account"
+                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                >
+                  Account
+                </Link>
+                <Link
                   href="/login"
                   className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                 >
